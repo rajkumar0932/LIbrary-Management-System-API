6ef2c17bf405eff6f9a6ac4bbaf40d9bfb59f7d6
raj gupta <kumarraj0932@gmail.com>
2026-07-14 05:57:35 +0530

    Remove docs and diagrams from repo

--- config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-// connect to mongodb. server.js awaits this and exits if it throws
-export const dbConnect = async () => {
-  const conn = await mongoose.connect(process.env.DATABASE_URL);
-  console.log(`MongoDB connected: ${conn.connection.host}`);
-};
